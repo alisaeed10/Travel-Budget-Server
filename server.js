5dbfc0d57f87e9586a37e8438ea19788b528cbbc
ali <4lisaeed96@gmail.com>
2023-12-28 22:04:58 -0600

    created .env file

--- server.js
+++ server.js
@@ -37,6 +37,12 @@ const getTokenParams = (code) => queryString.stringify({
     grant_type: 'authorization_code',
     redirect_uri: config.redirectUrl,
 });
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 app.set("trust proxy", 1);
 
